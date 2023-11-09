564f2f3df53623c7d3c656b62b5a94f68c386bb8
Arthur <arthur.jenck@epitech.eu>
2023-11-09 19:40:40 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movieweb.arthurjenckdev.workers.dev",
+  VITE_TMDB_READ_API_KEY:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTdiMDU3MWYyYWEzNjY4NjdkZTFjNzAwOTZiMTk2YiIsInN1YiI6IjY1NGI3MTJiMjkzODM1MDBhYjUyYWQ2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LMHq7NVpQU_3_yf-tEPt3SfHNPEbMxDAHX5LIPM-agg",
+};
